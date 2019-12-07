9ccfaa406f5d2e1d8e01e959d7ac4a35ff818752
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-12-07 19:16:01 +0100

    Add bin/run script, util crate reads proper input file

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -8,12 +8,19 @@ pub fn get_input() -> String {
     use std::io::Read;
     use std::path::PathBuf;
 
-    // let dir = if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
-    //     let executable = env::args().next().unwrap();
-    //     PathBuf::from(executable);
-    // };
+    let mut path = if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
+        PathBuf::from(manifest_dir)
+    } else {
+        let executable = env::args().next().unwrap();
+        PathBuf::from(executable)
+            .parent()
+            .map(Into::into)
+            .unwrap_or(PathBuf::from("."))
+    };
 
-    let mut file = File::open(INPUT_FILENAME).unwrap();
+    path.push(INPUT_FILENAME);
+
+    let mut file = File::open(path).unwrap();
     let mut content = String::new();
     file.read_to_string(&mut content).unwrap();
 
