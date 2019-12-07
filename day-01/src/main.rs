88285650385d5ebbf2262a5d96601dcc5d7d1638
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-12-07 19:39:23 +0100

    Day 01 complete

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -4,11 +4,20 @@
 extern crate aoc_util as util;
 
 fn main() {
-    use std::env;
-
-    dbg!(env::args().next());
-
     let input = util::get_input();
 
-    println!("Hello, world!");
+    let masses = input
+        .split("\n")
+        .filter_map(|line_s| line_s.parse::<i32>().ok())
+        .collect::<Vec<i32>>();
+
+    let fuel_requirements = masses.into_iter().map(calcu_fuel_for_mass);
+
+    let fuel_sum: i32 = fuel_requirements.sum();
+
+    println!("{}", fuel_sum);
+}
+
+fn calcu_fuel_for_mass(mass: i32) -> i32 {
+    mass / 3 - 2
 }
